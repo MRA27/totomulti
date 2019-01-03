537ac97cf6af789a98d81e218cce17c3775b8802
MRA27 <45831536+MRA27@users.noreply.github.com>
2019-01-04 04:33:34 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
+//jangan ada sepasi
+$config = [
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBin-Zr9CjbbSle8u8jXqyina7zo7chWS0iIVlkKO33IqKhM-wuoVptLiGlq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=942471dfcb423d90","ticket=sHp2r312otmCpnGns3mCqJhovNiBiojRrtCOpLO1c8-8o4awiZym2oK5hpq_nolnhI23l4uKe9yv0J9qtM57za2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=f23ba0379305109b"
+  ];
+
+?>
